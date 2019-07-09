4859c8b64e9ef63e7606eef3373e25746883741d
PascalUlor <pascalulor@yahoo.com>
2019-07-09 14:14:32 +0100

    get post by id

--- controllers/index.js
+++ controllers/index.js
@@ -20,5 +20,30 @@ const getPosts = async (req, res) => {
     });
   }
 };
+const getById = async (res, id, statusCode) => {
+  try {
+    const blogPost = await Blog.findById(id);
+    if (blogPost.length) {
+      return res.status(200).json({
+        status: statusCode,
+        data: blogPost
+      });
+    }
+    return res.status(404).json({
+      status: 404,
+      message: "The post with the specified ID does not exist."
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "The post with the specified ID does not exist."
+    });
+  }
+};
+const getPostById = (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  getById(res, id, 200);
+};
 
-module.exports = { getPosts };
+module.exports = { getPosts, getPostById };
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,6 @@ const route = express.Router();
 
 route.get("/", dbMethod.getPosts);
 
+route.get("/:id", dbMethod.getPostById);
+
 module.exports = route;
